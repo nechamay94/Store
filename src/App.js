dc5e28447c0e5521ff1f7460ae7eab5222782118
nechamay94 <50147100+nechamay94@users.noreply.github.com>
2020-06-23 14:25:51 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {FontAwesome} from "react-icons/fa"
+
+function App() {
+  return (
+    <div className="container" >
+          <div className="row" >
+            <div className="col-6">column number one</div>
+            <div className="col-6">
+              <span>
+                <i className="fas fa-home"/>
+                </span>
+                </div>
+
+          </div>
+    </div>
+    );
+}
+
+export default App;
